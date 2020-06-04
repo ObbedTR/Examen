3c7e4362763a35ea7a47a900573f73692b7dff90
ObbedTR <64038287+ObbedTR@users.noreply.github.com>
2020-06-03 18:44:31 -0600

    Delete

--- routes/artista.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-let artista = require('../controllers/artistController.js');
-
-let router = express.Router();
-
-router.get('/', artista.list);
-
-module.exports = router;
