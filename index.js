c756fb60e8c3fd2bbb8bf11306ed0237248bdfc5
ObbedTR <64038287+ObbedTR@users.noreply.github.com>
2020-06-03 20:36:08 -0600

    Update

--- index.js
+++ index.js
@@ -1,63 +1,99 @@
-//Se importa el paquete espress y el hbs
-const express =require('express');
-const hbs=require('hbs');
+{{> header}}
+    
+    <center><div><h1 style="font-size: 800%" >Pintoras Y Pintores</h1></div></center>
+<br><br><br>
 
-//Se crea la app
-const app = express();
-//Se define la fuente de platillas
-app.set('view engine', 'hbs');
+<div class="card mb-3" style="max-heihgt: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://pbs.twimg.com/media/DRWvSy3XUAAq_7y.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <a href="/koi"><h5 class="card-title">Koi Samsa</h5> </a>
+        <p class="card-text">
+            _id: 5ed8175a91abbafcb010a22e <br>
+            Española<br>
+            Patricia es diseñadora especializada en branding, y comunicación corporativa, docente, twittera, directora de arte, vlogger… Esto es sólo una parte de lo que es Koi Samsa.
+            Actual Talent Manager de Tuiwok Estudios, se dedica a encontrar y potenciar talento en YouTube. </p>
+        <p class="card-text"><small class="text-muted"><em>"...la palabra y el discurso son imprescindibles en todo lo que hago..."</em></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+           
+<div class="card mb-3" style="max-heihgt: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://images.squarespace-cdn.com/content/v1/58b1be81d1758e01328ce8bb/1561988015794-Y5L9VMWGWM1ANWYQFBZE/ke17ZwdGBToddI8pDm48kF8BL7ExAzp1lS4ODMnDRmxZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzJS46bAGw0Mcvc3hYFlJZoy81S2xjX169l3_O9Qykt-9FwdHWSXQnLT9Hyk6rbhDY/AM-DeBrincat_BloomingSky.jpg?format=750w" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+<a href="/am">        <h5 class="card-title">AM DeBrincat (Karen Mainenti)</h5>
+</a>        <p class="card-text">
+            _id: 5ed8175a91abbafcb010a22f <br>
+            Estadounidense <br>
+            Corriente: Moderno-Mixed Media Technique <br>
+            Ha sido artista residente en el Museo de Arte Contemporáneo de Massachusetts (MASS MoCA), Chashama y el Proyecto Wassaic, y ha recibido subvenciones y fondos del Museo de Arte Contemporáneo de Massachusetts, la Fundación Durst y el Fondo Deming.
+        <p class="card-text"><small class="text-muted"><em>"But now when I see it, I just think of meditation and how much peace and joy it’s brought me."</em></small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
-//Se generan los Partials
-hbs.registerPartial('partial', (__dirname + '/views/partial.hbs', 'utf8'));
-hbs.registerPartials(__dirname + '/views/partials/')
-//Sitio estático
-app.use(express.static(__dirname + '/public'));
+<div class="card mb-3" style="max-heihgt: 540px;">
+<div class="row no-gutters">
+  <div class="col-md-4">
+    <img src="https://static.wixstatic.com/media/defe22_2b3e6514a869472e8cf4a16827dec68e~mv2_d_2774_2119_s_2.jpg/v1/fill/w_635,h_485,al_c,q_90,usm_0.66_1.00_0.01/defe22_2b3e6514a869472e8cf4a16827dec68e~mv2_d_2774_2119_s_2.webp" class="card-img" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+<a href="/elly">      <h5 class="card-title">Elly Smallwood</h5>
+</a>      <p class="card-text">
+          _id: 5ed8175a91abbafcb010a230<br>
+          Canadiense <br>
+          Corriente: Moderno-Impresionismo <br>
+          Se graduó con su BFA en Dibujo y Pintura en 2011. Sus pinturas son intensamente personales, una exploración visual de su mente y cuerpo, y los de las personas que la rodean.
+      <p class="card-text"><small class="text-muted"><em>"I aim to break down my own personal boundaries and push myself to use abandon when I would normally use caution, to destroy those marks which I begin to hold as precious. These pieces are an exploration of the innocent and the grotesque; delicate figures become unnerving and soft colours are smeared across figures. In my works I see a twisted view of self, a visceral representation of how I view humanity, particularly in regards to my own body."</em></small></p>
+    </div>
+  </div>
+</div>
+</div>
 
-//Configuración de rutas
-app.get('/',(req, res)=>{
-    res.render('index',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'Inicio'
-    });
-});
-app.get('/koi',(req, res)=>{
-    res.render('koi',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'Koi Samnsa'
-    });
-});
-app.get('/AM',(req, res)=>{
-    res.render('AM',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'AM DeBrincat'
-    });
-});
-app.get('/elly',(req, res)=>{
-    res.render('elly',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'Elly Smallwood'
-    });
-});
-app.get('/jaime',(req, res)=>{
-    res.render('jaime',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'Jaime Ruíz'
-    });
-});
-app.get('/liam',(req, res)=>{
-    res.render('liam',{
-        autor: 'Jaime Obbed Tarango Ramírez 4°E',
-        year: new Date().getFullYear(),
-        tittle: 'La Rouille'
-    });
-});
+<div class="card mb-3" style="max-heihgt: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/edf00730248953.561a99826290b.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+<a href="/jaime">        <h5 class="card-title">Jaime Ruíz</h5>
+</a>        <p class="card-text">
+            _id: 5ed8175a91abbafcb010a231<br>
+            Colombiano <br>
+            Corriente: Impresionismo-Charcoal <br>
+        <p class="card-text"><small class="text-muted"><em>"La abstracción, la disminución a los rasgos fisionómicos, es sin lugar a duda un intento por transcribir el cúmulo de sensaciones que el individuo retratado suscita en mí, y esta reacción emocional y subjetiva condiciona el conjunto de las formas de cada pintura dentro de un compendio lingüístico determinado. Busco además transformar el estereotipo de un retrato convencional, que únicamente simula la individualidad concreta en una imagen, proceso donde el rostro humano aparece en los límites de su disolución."</em></small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
-//Se incia el servidor web
-app.listen(3000, ()=>{
-    console.log('Escuchando el puerto 3000')
-})
+<div class="card mb-3" style="max-heihgt: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f6b7a440777793.578d1c36cc12d.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+<a href="/liam">        <h5 class="card-title">La Rouille (Liam Decay)</h5>
+</a>        <p class="card-text">
+            _id: 5ed8175a91abbafcb010a232<br>
+            Frances <br>
+            Corriente: Moderno-Charcoal<br>
+            Autodidacta, La rouille descubrió la pintura tarde en las exploraciones urbanas. Atraído por las atmósferas únicas que emergen de estos espacios urbanos abandonados por el hombre, su enfoque se centra en el daño relacionado con la historia y el tiempo, así como con el físico y el memorial.
+        <p class="card-text"><small class="text-muted"><em>"The silhouettes bring to life for a brief moment, to these forgotten places. Often resonate with his own ghosts and those who haunt these various locations."</em></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+{{> footer}}
